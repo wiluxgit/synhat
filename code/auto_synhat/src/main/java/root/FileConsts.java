151e14efe03b27a666a48b305a49690294553bd8
Oscar Dahlqvist <oscdah@student.chalmers.se>
2021-03-20 20:21:22 +0100

    rm gitignore

--- code/auto_synhat/src/main/java/root/FileConsts.java
+++ code/auto_synhat/src/main/java/root/FileConsts.java
@@ -7,7 +7,6 @@ public final class FileConsts {
     public static final File downloadDir = new File("resourcepacks/synhat/temp");
     public static final File assetsDir = new File(downloadDir, "synhat-master/assets");
     public static final File outputDir = new File("resourcepacks/synhat");
-    public static final File modelDeclrFile = new File(assetsDir,"%INTERNAL/perm.json");
     public static final File permanentStorageFile = new File(outputDir,"properties.json");
 
 }
