465582f6bac03973243d117f8088d852bc3b7bde
Wiluxgit <wilux00@gmail.com>
2025-08-04 18:49:41 +0200

    update give item to 21.1

--- scripts/give_items/give_items.py
+++ scripts/give_items/give_items.py
@@ -14,28 +14,34 @@ def run(path = "../__master_models_table.json"):
         for mcItem in allMcItems:
             mcItems2Hat[mcItem] = [x for x in hatList if x["item"] == mcItem]
 
-        #mcItems2Hat["minecraft:netherite_hoe"]
-
         chestList = []
         for mcItem,hatList in mcItems2Hat.items():
             itemList = [makeItem(hd) for hd in hatList]
             chestList.append(makeMegaChest(mcItem, itemList))
-        masterChest = makeMegaChest("MasterChest", chestList)
+        NBT_masterChest = makeMegaChest("MasterChest", chestList)
 
-        #chestMeta = nbt.from_json(masterChest)
-        jsonDict = masterChest["tag"]
+        nbt = components_to_nbt(NBT_masterChest)
+        print(f"give Wilux white_shulker_box{nbt}")
 
-        #print(json.dumps(jsonDict,indent=2))
+def to_nbt(obj):
+    if isinstance(obj, dict):
+        return '{' + ','.join(f'{k}:{to_nbt(v)}' for k, v in obj.items()) + '}'
+    elif isinstance(obj, list):
+        return '[' + ','.join(to_nbt(i) for i in obj) + ']'
+    elif isinstance(obj, str):
+        escaped = obj.encode('unicode_escape').decode('ascii')
+        escaped = escaped.replace("'", "\\'")
+        return "'" + escaped + "'"
+    else:
+        return repr(obj)
+def components_to_nbt(obj):
+    content = ','.join(
+        f'{k}={to_nbt(v)}'
+        for k, v
+        in obj["components"].items()
+    )
+    return '[' + content + ']'
 
-        nbt = jsonDict2NBTText(jsonDict)
-        print(f"give Wilux chest{nbt}")
-
-def jsonDict2NBTText(jsonDict):
-    jsonStr = json.dumps(jsonDict, separators=(',', ':'))
-    rtxt = re.sub(r'"(\w+)"\s*:', lambda n:re.sub(r'"','', n.group()), jsonStr)
-    rtxt = re.sub(r'(?<=[^\\])"',r"'",rtxt)
-    txt = rtxt.encode("ascii").decode("unicode-escape",'backslashreplace')
-    return txt
 
 def chunk(l, n):
     return [l[i:i + n] for i in range(0, len(l), n)]
@@ -50,49 +56,54 @@ def makeMegaChest(categoryStr, itemList):
 def makeChest(categoryStr, itemList):
     if len(itemList) > 27: raise Exception("Chest can not contain more than 27 items")
 
-    mcItemBox = stringToShulkerBox(categoryStr)
-
+    NBT_container = []
     for i,item in enumerate(itemList):
-        item["Slot"] = i
+        NBT_container.append({
+            "slot": i,
+            "item": item,
+        })
 
-    chest = {"id":mcItemBox, "Count":1, "tag":{
-        "display":{"Name":f'{{"text":"{categoryStr}"}}'},
-        "BlockEntityTag":{"Items":itemList}
-    }}
+    chest = {
+        "id": stringToShulkerBox(categoryStr),
+        "components": {
+            "custom_name": f'{{"text":"{categoryStr}"}}',
+            "container": NBT_container,
+        }
+    }
     return chest
 
 def stringToShulkerBox(str):
     x = hash(str)%17
     cols = ["","white_","orange_","magenta_","light_blue_","yellow_","lime_","pink_","gray_","light_gray_","cyan_","purple_","blue_","brown_","green_","red_","black_"]
-    return f"minecraft:{cols[x]}shulker_box"
+    return f"{cols[x]}shulker_box"
 
 def makeItem(hd):
+    def jsonEscape(s: str) -> str:
+        return json.dumps(s)[1:-1]
+
     mcItem = hd["item"]
     cmData = hd["data"]
     path = hd["model"]
     displayName = hd["displayName"]
-    displayName = re.sub(r"'",r"\'",displayName)
 
     color = "yellow"
     if "playerRank" in hd:
-        rankCol = {"T":"dark_aqua", "A":"blue", "H":"light_blue", "D":"gold"}
+        rankCol = {"T":"dark_aqua", "A":"blue", "H":"aqua", "D":"gold"}
         color = rankCol[hd["playerRank"]]
 
-    disp = {
-        "Name":f'{{"text":"{displayName}","italic":false,"color":"{color}"}}',
-        "Lore":[f'{{"text":"id:{cmData}","italic":false,"color":"dark_gray"}}']
-    }
+    NBT_custom_name = f'{{"text":"{jsonEscape(displayName)}","color":"{color}"}}'
+    NBT_lore = [f'{{"text":"id:{cmData}","color":"dark_gray"}}']
     if "lore" in hd:
         lore = hd["lore"]
-        lore = re.sub(r"'",r"\'",lore)
-        disp["Lore"].insert(0,f'{{"text":"\\\\"{lore}\\\\"","color":"gray"}}')
+        NBT_lore.insert(0,f'{{"text":"{jsonEscape(lore)}","color":"gray"}}')
 
     item = {
-        "id":mcItem,
-        "Count":1,
-        "tag":{
-            "display":disp,
-            "CustomModelData":cmData
+        "id": mcItem,
+        "count":1,
+        "components": {
+            "custom_model_data": cmData,
+            "item_name": NBT_custom_name,
+            "lore": NBT_lore,
         }
     }
     return item
