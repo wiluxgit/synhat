3fd6e84a251c7c83a162fdab67291e4f559851c2
Wiluxgit <wilux00@gmail.com>
2025-08-06 17:38:13 +0200

    make_player_obj: add Alex support (needed to reorder UV positions since bottom face is not alligned with arm on alex)

--- .github/workflows/scripts/make_player/make_player_obj.py
+++ .github/workflows/scripts/make_player/make_player_obj.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Generator, TextIO
+from typing import Generator, Self, TextIO
 import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
@@ -48,36 +48,46 @@ class BodyPart:
 
 @dataclass
 class ObjCounters:
-    vid: int
-    vtid: int
-    vnid: int
+    v: int
+    vt: int
+    vn: int
     fid: int
 
+OVERLAY_SCALE = 1.125
+PARTS_STEVE = [
+#            name     uv TL     size      3d center  3d scale
+    BodyPart("Head",  ( 0, 0),  (8, 8,8), ( 0,28,0), 1),
+    BodyPart("Body",  (16,16),  (8,12,4), ( 0,18,0), 1),
+    BodyPart("LArm",  (40,16),  (4,12,4), (-6,18,0), 1),
+    BodyPart("RArm",  (32,48),  (4,12,4), ( 6,18,0), 1),
+    BodyPart("LLeg",  ( 0,16),  (4,12,4), (-2, 6,0), 1),
+    BodyPart("RLeg",  (16,48),  (4,12,4), ( 2, 6,0), 1),
+    BodyPart("oHead", (32, 0),  (8, 8,8), ( 0,28,0), OVERLAY_SCALE),
+    BodyPart("oRLeg", ( 0, 48), (4,12,4), ( 2, 6,0), OVERLAY_SCALE),
+    BodyPart("oLLeg", ( 0, 32), (4,12,4), (-2, 6,0), OVERLAY_SCALE),
+    BodyPart("oRArm", (48, 48), (4,12,4), ( 6,18,0), OVERLAY_SCALE),
+    BodyPart("oLArm", (40, 32), (4,12,4), (-6,18,0), OVERLAY_SCALE),
+    BodyPart("oBody", (16, 32), (8,12,4), ( 0,18,0), OVERLAY_SCALE),
+]
+PARTS_ALEX = [
+    BodyPart("Head",  ( 0, 0),  (8, 8,8), ( 0,28,0), 1),
+    BodyPart("Body",  (16,16),  (8,12,4), ( 0,18,0), 1),
+    BodyPart("LArm",  (40,16),  (3,12,4), (-6,18,0), 1),
+    BodyPart("RArm",  (32,48),  (3,12,4), ( 6,18,0), 1),
+    BodyPart("LLeg",  ( 0,16),  (4,12,4), (-2, 6,0), 1),
+    BodyPart("RLeg",  (16,48),  (4,12,4), ( 2, 6,0), 1),
+    BodyPart("oHead", (32, 0),  (8, 8,8), ( 0,28,0), OVERLAY_SCALE),
+    BodyPart("oRLeg", ( 0, 48), (4,12,4), ( 2, 6,0), OVERLAY_SCALE),
+    BodyPart("oLLeg", ( 0, 32), (4,12,4), (-2, 6,0), OVERLAY_SCALE),
+    BodyPart("oRArm", (48, 48), (3,12,4), ( 6,18,0), OVERLAY_SCALE),
+    BodyPart("oLArm", (40, 32), (3,12,4), (-6,18,0), OVERLAY_SCALE),
+    BodyPart("oBody", (16, 32), (8,12,4), ( 0,18,0), OVERLAY_SCALE),
+]
+
 def run():
-    # Overlay parts (slightly bigger)
-    OVERLAY_SCALE = 1.125
+    parts = PARTS_STEVE
 
-
-    parts = [
-    #            name     uv TL     size      3d center  3d scale
-        BodyPart("Head",  ( 0, 0),  (8, 8,8), ( 0,28,0), 1),
-        BodyPart("Body",  (16,16),  (8,12,4), ( 0,18,0), 1),
-        BodyPart("LArm",  (40,16),  (4,12,4), (-6,18,0), 1),
-        BodyPart("RArm",  (32,48),  (4,12,4), ( 6,18,0), 1),
-        BodyPart("LLeg",  ( 0,16),  (4,12,4), (-2, 6,0), 1),
-        BodyPart("RLeg",  (16,48),  (4,12,4), ( 2, 6,0), 1),
-
-        BodyPart("oHead", (32, 0),  (8, 8,8), ( 0,28,0), OVERLAY_SCALE),
-        BodyPart("oRLeg", ( 0, 48), (4,12,4), ( 2,6 ,0), OVERLAY_SCALE),
-        BodyPart("oLLeg", ( 0, 32), (4,12,4), (-2, 6,0), OVERLAY_SCALE),
-        BodyPart("oRArm", (48, 48), (4,12,4), ( 6,18,0), OVERLAY_SCALE),
-        BodyPart("oLArm", (40, 32), (4,12,4), (-6,18,0), OVERLAY_SCALE),
-        BodyPart("oBody", (16, 32), (8,12,4), ( 0,18,0), OVERLAY_SCALE),
-    ]
-
-    #generateUV2FaceidCode(parts)
-    #generateAreaToFaceCode(parts)
-
+    # Handrolling some .obj file because we need very specific vertex ordering
     with open("C:/Users/wilux/AppData/Roaming/.minecraft/resourcepacks/synhat-dev/web_editor/assets/steve.obj", "w+") as f:
         f.write("# Made by Wilux\n")
         f.write("mtllib steve.mtl\n\n")
@@ -89,34 +99,31 @@ def run():
             writeCube(f, part, counters)
 
 def writeCube(f: TextIO, part: BodyPart, counters: ObjCounters):
-    """ Handrolling some .obj file because we need very specific vertex ordering """
     file_v = []
     file_vt = []
     file_vn = []
     file_f = []
 
-    cornUvs = body_cornerUvsNormalized(part)
-    for cuv in cornUvs:
-        counters.vtid += 1
-        file_vt.append(f"vt {cuv[0]} {cuv[1]}\n")
+    for u, v in body_cornerUvsNormalized(part):
+        file_vt.append(f"vt {u} {v}\n")
+        counters.vt += 1
 
-    for dirid, relativeVertexIds in DIRECTION_2_VERTEXORDER.items():
+    for dir, relativeVertexIds in DIRECTION_2_VERTEXORDER.items():
         for vidx in relativeVertexIds:
             vcoord = body_toCubeCoords(part)[vidx]/32
             file_v.append(f"v {vcoord[0]} {vcoord[1]} {vcoord[2]}\n")
+            counters.v += 1
 
-            counters.vid += 1
-
-        fnorm = faceDir_normal(dirid)
+        fnorm = faceDir_normal(dir)
         file_vn.append(f"vn {fnorm[0]} {fnorm[1]} {fnorm[2]}\n")
-        counters.vnid += 1
+        counters.vn += 1
 
-        xt = counters.vtid-13
-        xn = counters.vnid-1
-        xv = counters.vid-4
+        # Bind UV and normals to vt:s UV
+        xt = counters.vt-UV_ORDER_COUNT # get the index for the first vt for this shape
+        xn = counters.vn-1              # get the index for the first vn for this face
+        xv = counters.v-4               # get the index for the first v for this face
 
-        c0, c1, c2, c3 = faceDir_counterClockwiseCornerUVIds(dirid, xt)
-
+        c0, c1, c2, c3 = faceDir_counterClockwiseCornerUVIds(dir, xt)
         file_f.append(
             f"f {xv}/{c0}/{xn} {xv+1}/{c1}/{xn} {xv+2}/{c2}/{xn}\n"
         )
@@ -150,39 +157,54 @@ def faceDir_normal(self: FaceDirection) -> NDArray[np.floating]:
             ret = np.array([0,0,-1]) # back
     return ret*0.555
 
+"""
+UV ordering is an arbitrary ordering by me, it does not correspond to any minecraft property
+#       ^ v (up)
+#
+#           x   x
+#         0---1---2
+#         |   |   |
+# z       | 0 | 1 |
+#         |   |   |
+#         3---4---5
+#   6-----7---8-----9---10    -> u (left)
+# y |  2  | 3 |  4  | 5 |
+#   11----12--13----14--15
+#      z    x    z    x
+"""
+UV_ORDER_COUNT = 16
 def faceDir_counterClockwiseCornerUVIds(self: FaceDirection, offset) -> Generator[int, None, None]:
+    """ The starting point of the indexing must be corner 0 see gl_VertexID.png """
     match self:
         case FaceDirection.TOP:
-            return (offset + i for i in (1,0,4,5)) # top
+            return (offset + i for i in (1, 0, 3, 4))
         case FaceDirection.BOTTOM:
-            return (offset + i for i in (6,5,1,2)) # bottom
+            return (offset + i for i in (5, 4, 1, 2))
         case FaceDirection.RIGHT:
-            return (offset + i for i in (4,3,8,9)) # right
+            return (offset + i for i in (7, 6, 11, 12))
         case FaceDirection.FRONT:
-            return (offset + i for i in (5,4,9,10)) # front
+            return (offset + i for i in (8, 7, 12, 13))
         case FaceDirection.LEFT:
-            return (offset + i for i in (6,5,10,11)) # left
+            return (offset + i for i in (9, 8, 13, 14))
         case FaceDirection.BACK:
-            return (offset + i for i in (7,6,11,12)) # back
+            return (offset + i for i in (10, 9, 14, 15))
 
 def body_cornerUvs(self: BodyPart) -> list[tuple[int,int]]:
-    """ returns an iterator for the UV in pixel from the top left
-    #       ^ v (up)
-    #
-    #       0---1---2
-    #       | 0 | 1 |
-    #   3---4---5---6---7    -> u (left)
-    #   | 2 | 3 | 4 | 5 |
-    #   8---9---10--11--12
-    """
+    """ Returns an iterator for the UV as [u,v] tuples in units of pixels from the top left
+        Sorted in UV ordering """
     x = self.axis_sizes[0]
     y = self.axis_sizes[1]
     z = self.axis_sizes[2]
     (ou, ov) = self.origin_uv
     uvpx = [
+        # TOP/BOTTOM
         (ou + z,             ov),
         (ou + z + x ,        ov),
         (ou + z + x + x ,    ov),
+        (ou + z,             ov + z),
+        (ou + z + x ,        ov + z),
+        (ou + z + x + x ,    ov + z),
+        # RIGHT/FRONT/LEFT/BACK
         (ou,                 ov + z),
         (ou + z,             ov + z),
         (ou + z + x,         ov + z),
