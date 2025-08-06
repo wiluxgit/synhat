676be7e3999e3642b562ac2749ed9a25e1bd4e56
Wiluxgit <wilux00@gmail.com>
2025-08-06 16:29:26 +0200

    rewrite make_player_obj to be more modular

--- .github/workflows/scripts/make_player/make_player_obj.py
+++ .github/workflows/scripts/make_player/make_player_obj.py
@@ -1,22 +1,19 @@
-from typing import Generator
+from enum import Enum
+from typing import Generator, TextIO
 import numpy as np
 from numpy.typing import NDArray
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
 
-partname_head = "Head"
-partname_body = "Body"
-partname_rarm = "RArm"
-partname_larm = "LArm"
-partname_rleg = "RLeg"
-partname_lleg = "LLeg"
-partname_ohead = "oHead"
-partname_orleg = "oRLeg"
-partname_olleg = "oLLeg"
-partname_orarm = "oRArm"
-partname_olarm = "oLArm"
-partname_obody = "oBody"
+class FaceDirection(Enum):
+    TOP = 0
+    BOTTOM = 1
+    RIGHT = 2
+    FRONT = 3
+    LEFT = 4
+    BACK = 5
 
-def run():
-#===================================
+""" DIRECTION_2_VERTEXORDER uses the following corner numbering
 #     ^ Up (+Y)    Towards Face (+Z)
 #                 /
 #     5------4
@@ -27,184 +24,161 @@ def run():
 #   |/     |/
 #   2------3
 #
-#===================================
-#       ^ v (up)
-#
-#       0---1---2
-#       | 2 | 3 |
-#   3---4---5---6---7    -> u (left)
-#   | 1 | 4 | 0 | 5 |
-#   8---9---10--11--12
-#===================================
-#    0: left
-#    1: right
-#    2: top
-#    3: bottom
-#    4: front
-#    5: back
-#===================================
-#
-# order: [head, body, rarm, larm, rleg, lleg, ohead, olleg, orleg, orarm, olarm, obody]
-#
-    ordfaces = [
-        (4,5,1,0), # top
-        (3,2,6,7), # bottom
-        (1,5,6,2), # right
-        (0,1,2,3), # front
-        (4,0,3,7), # left
-        (5,4,7,6), # back
-    ]
+"""
+DIRECTION_2_VERTEXORDER = {
+    FaceDirection.TOP:    (4,5,1,0),
+    FaceDirection.BOTTOM: (3,2,6,7),
+    FaceDirection.RIGHT:  (1,5,6,2),
+    FaceDirection.FRONT:  (0,1,2,3),
+    FaceDirection.LEFT:   (4,0,3,7),
+    FaceDirection.BACK:   (5,4,7,6),
+}
 
-    pix1 = np.array([
-        [1,1,1],
-        [0,1,1],
-        [0,0,1],
-        [1,0,1],
-        [1,1,0],
-        [0,1,0],
-        [0,0,0],
-        [1,0,0],
-    ]) - np.array([0.5,0.5,0.5])
+@dataclass
+class BodyPart:
+    name: str
+    # TOP LEFT of the UV square
+    origin_uv: tuple[int,int]
+    # width,height,depth (x,y,z)
+    axis_sizes: tuple[int,int,int]
+    # 3d CENTER of the cuboid
+    origin_3d_center: tuple[int,int,int]
+    #
+    scale_3d: float
 
-    center_head = np.array([ 0,28,0])
-    center_body = np.array([ 0,18,0])
-    center_rarm = np.array([ 6,18,0])
-    center_larm = np.array([-6,18,0])
-    center_rleg = np.array([ 2,6 ,0])
-    center_lleg = np.array([-2,6 ,0])
+@dataclass
+class ObjCounters:
+    vid: int
+    vtid: int
+    vnid: int
+    fid: int
 
-    head = center_head + mulin(pix1, [8,8,8])
-    body = center_body + mulin(pix1, [8,12,4])
-    rarm = center_rarm + mulin(pix1, [4,12,4])
-    larm = center_larm + mulin(pix1, [4,12,4])
-    rleg = center_rleg + mulin(pix1, [4,12,4])
-    lleg = center_lleg + mulin(pix1, [4,12,4])
+def run():
+    # Overlay parts (slightly bigger)
+    OVERLAY_SCALE = 1.125
 
-    overlayscale = 0.28125/0.25
-    ohead = center_head + mulin(pix1, [8,8,8]) * overlayscale
-    olleg = center_lleg + mulin(pix1, [4,12,4]) * overlayscale
-    orleg = center_rleg + mulin(pix1, [4,12,4]) * overlayscale
-    orarm = center_rarm + mulin(pix1, [4,12,4]) * overlayscale
-    olarm = center_larm + mulin(pix1, [4,12,4]) * overlayscale
-    obody = center_body + mulin(pix1, [8,12,4]) * overlayscale
-
-    origin_head = (0,0)
-    origin_body = (16,16)
-    origin_rarm = (32,48)
-    origin_larm = (40,16)
-    origin_rleg = (16,48)
-    origin_lleg = (0,16)
-
-    origin_ohead = (32, 0)
-    origin_olleg = (0, 32)
-    origin_orleg = (0, 48)
-    origin_orarm = (48, 48)
-    origin_olarm = (40, 32)
-    origin_obody = (16, 32)
 
     parts = [
-        (partname_head, head, origin_head),
-        (partname_body, body, origin_body),
-        (partname_larm, larm, origin_larm),
-        (partname_rarm, rarm, origin_rarm),
-        (partname_lleg, lleg, origin_lleg),
-        (partname_rleg, rleg, origin_rleg),
-        (partname_ohead, ohead, origin_ohead),
-        (partname_orleg, orleg, origin_orleg),
-        (partname_olleg, olleg, origin_olleg),
-        (partname_orarm, orarm, origin_orarm),
-        (partname_olarm, olarm, origin_olarm),
-        (partname_obody, obody, origin_obody),
+    #            name     uv TL     size      3d center  3d scale
+        BodyPart("Head",  ( 0, 0),  (8, 8,8), ( 0,28,0), 1),
+        BodyPart("Body",  (16,16),  (8,12,4), ( 0,18,0), 1),
+        BodyPart("LArm",  (40,16),  (4,12,4), (-6,18,0), 1),
+        BodyPart("RArm",  (32,48),  (4,12,4), ( 6,18,0), 1),
+        BodyPart("LLeg",  ( 0,16),  (4,12,4), (-2, 6,0), 1),
+        BodyPart("RLeg",  (16,48),  (4,12,4), ( 2, 6,0), 1),
+
+        BodyPart("oHead", (32, 0),  (8, 8,8), ( 0,28,0), OVERLAY_SCALE),
+        BodyPart("oRLeg", ( 0, 48), (4,12,4), ( 2,6 ,0), OVERLAY_SCALE),
+        BodyPart("oLLeg", ( 0, 32), (4,12,4), (-2, 6,0), OVERLAY_SCALE),
+        BodyPart("oRArm", (48, 48), (4,12,4), ( 6,18,0), OVERLAY_SCALE),
+        BodyPart("oLArm", (40, 32), (4,12,4), (-6,18,0), OVERLAY_SCALE),
+        BodyPart("oBody", (16, 32), (8,12,4), ( 0,18,0), OVERLAY_SCALE),
     ]
-    generateUV2FaceidCode(parts)
-    generateAreaToFaceCode(parts)
+
+    #generateUV2FaceidCode(parts)
+    #generateAreaToFaceCode(parts)
 
     with open("C:/Users/wilux/AppData/Roaming/.minecraft/resourcepacks/synhat-dev/web_editor/assets/steve.obj", "w+") as f:
         f.write("# Made by Wilux\n")
         f.write("mtllib steve.mtl\n\n")
 
-        _vid, _vtid, _vnid, _fid = (1,1,1,1)
+        counters = ObjCounters(1,1,1,1)
 
-        for (name, cube, texorigin) in parts:
+        for part in parts:
             #f.write(f"o {name}\n")
-            _vid, _vtid, _vnid, _fid = writeCube(f, ordfaces,
-                cube=cube, texOrg=texorigin, partName=name,
-                vid=_vid, vtid=_vtid, vnid=_vnid, fid=_fid
-            )
+            writeCube(f, part, counters)
 
-def writeCube(f, ordfaces, cube, texOrg, partName, vid, vtid, vnid, fid):
-
+def writeCube(f: TextIO, part: BodyPart, counters: ObjCounters):
+    """ Handrolling some .obj file because we need very specific vertex ordering """
     file_v = []
     file_vt = []
     file_vn = []
     file_f = []
 
-    cornUvs = corverUvs(texOrg, partName)
+    cornUvs = body_cornerUvsNormalized(part)
     for cuv in cornUvs:
-        vtid += 1
+        counters.vtid += 1
         file_vt.append(f"vt {cuv[0]} {cuv[1]}\n")
 
-    for dirid, ordface in enumerate(ordfaces):
-        for vidx in ordface: #vertex id
-            vcoord = cube[vidx]/32
+    for dirid, relativeVertexIds in DIRECTION_2_VERTEXORDER.items():
+        for vidx in relativeVertexIds:
+            vcoord = body_toCubeCoords(part)[vidx]/32
             file_v.append(f"v {vcoord[0]} {vcoord[1]} {vcoord[2]}\n")
 
-            vid += 1
+            counters.vid += 1
 
-        fnorm = faceNormal(dirid)
+        fnorm = faceDir_normal(dirid)
         file_vn.append(f"vn {fnorm[0]} {fnorm[1]} {fnorm[2]}\n")
-        vnid += 1
+        counters.vnid += 1
 
-        xt = vtid-13
-        xn = vnid-1
-        xv = vid-4
+        xt = counters.vtid-13
+        xn = counters.vnid-1
+        xv = counters.vid-4
 
-        c0, c1, c2, c3 = counterClockwiseCornerUVIds(dirid, xt)
-
+        c0, c1, c2, c3 = faceDir_counterClockwiseCornerUVIds(dirid, xt)
 
         file_f.append(
             f"f {xv}/{c0}/{xn} {xv+1}/{c1}/{xn} {xv+2}/{c2}/{xn}\n"
         )
-        fid += 1
+        counters.fid += 1
         file_f.append(
             f"f {xv}/{c0}/{xn} {xv+2}/{c2}/{xn} {xv+3}/{c3}/{xn}\n"
         )
-        fid += 1
+        counters.fid += 1
 
     f.writelines(file_v)
     f.writelines(file_vt)
     f.writelines(file_vn)
     f.write("usemtl m_32a6897e-4c41-36a4-cc6b-d8aec3b361de\n")
     f.writelines(file_f)
+    #f.write(f"fv {vcoord[0]} {vcoord[1]} {vcoord[2]}\n")
+    #f.write(f"fv {vcoord[0]} {vcoord[2]} {vcoord[3]}\n")
 
-    return vid, vtid, vnid, fid
+def faceDir_normal(self: FaceDirection) -> NDArray[np.floating]:
+    match self:
+        case FaceDirection.TOP:
+            ret = np.array([0,1,0]) # top
+        case FaceDirection.BOTTOM:
+            ret = np.array([0,-1,0]) # bottom
+        case FaceDirection.RIGHT:
+            ret = np.array([-1,0,0]) # right
+        case FaceDirection.FRONT:
+            ret = np.array([0,0,1]) # front
+        case FaceDirection.LEFT:
+            ret = np.array([1,0,0]) # left
+        case FaceDirection.BACK:
+            ret = np.array([0,0,-1]) # back
+    return ret*0.555
 
-        #f.write(f"fv {vcoord[0]} {vcoord[1]} {vcoord[2]}\n")
-        #f.write(f"fv {vcoord[0]} {vcoord[2]} {vcoord[3]}\n")
+def faceDir_counterClockwiseCornerUVIds(self: FaceDirection, offset) -> Generator[int, None, None]:
+    match self:
+        case FaceDirection.TOP:
+            return (offset + i for i in (1,0,4,5)) # top
+        case FaceDirection.BOTTOM:
+            return (offset + i for i in (6,5,1,2)) # bottom
+        case FaceDirection.RIGHT:
+            return (offset + i for i in (4,3,8,9)) # right
+        case FaceDirection.FRONT:
+            return (offset + i for i in (5,4,9,10)) # front
+        case FaceDirection.LEFT:
+            return (offset + i for i in (6,5,10,11)) # left
+        case FaceDirection.BACK:
+            return (offset + i for i in (7,6,11,12)) # back
 
-def counterClockwiseCornerUVIds(faceid, offset) -> Generator[int, None, None]:
-    i = faceid
-    if i == 0:
-        return (offset + i for i in (1,0,4,5)) # top
-    elif i == 1:
-        return (offset + i for i in (6,5,1,2)) # bottom
-    elif i == 2:
-        return (offset + i for i in (4,3,8,9)) # right
-    elif i == 3:
-        return (offset + i for i in (5,4,9,10)) # front
-    elif i == 4:
-        return (offset + i for i in (6,5,10,11)) # left
-    elif i == 5:
-        return (offset + i for i in (7,6,11,12)) # back
-    else:
-        raise Exception(f"{faceid=}")
-
-def corverUvs(texOrigin, partname):
-    size = cubeSizeInPixels(partname)
-    x = size[0]
-    y = size[1]
-    z = size[2]
-    (ou, ov) = texOrigin
+def body_cornerUvs(self: BodyPart) -> list[tuple[int,int]]:
+    """ returns an iterator for the UV in pixel from the top left
+    #       ^ v (up)
+    #
+    #       0---1---2
+    #       | 0 | 1 |
+    #   3---4---5---6---7    -> u (left)
+    #   | 2 | 3 | 4 | 5 |
+    #   8---9---10--11--12
+    """
+    x = self.axis_sizes[0]
+    y = self.axis_sizes[1]
+    z = self.axis_sizes[2]
+    (ou, ov) = self.origin_uv
     uvpx = [
         (ou + z,             ov),
         (ou + z + x ,        ov),
@@ -220,84 +194,26 @@ def corverUvs(texOrigin, partname):
         (ou + z + x + z,     ov + z + y),
         (ou + z + x + z + x, ov + z + y),
     ]
-    return [(u/64, 1-v/64) for (u,v) in uvpx]
-
-def cubeSizeInPixels(partname):
-    if partname in [partname_head, partname_ohead]:
-        return np.array([8,8,8])
-    elif partname in [partname_body, partname_obody]:
-        return np.array([8,12,4])
-    elif partname in [
-        partname_larm, partname_olarm, partname_lleg, partname_olleg,
-        partname_rarm, partname_orarm, partname_rleg, partname_orleg
-        ]:
-        return np.array([4,12,4])
-    raise ValueError(f"{partname} is not a known part")
-
-def faceNormal(faceid) -> NDArray[np.floating]:
-    match faceid:
-        case 0:
-            ret = np.array([0,1,0]) # top
-        case 1:
-            ret = np.array([0,-1,0]) # bottom
-        case 2:
-            ret = np.array([-1,0,0]) # right
-        case 3:
-            ret = np.array([0,0,1]) # front
-        case 4:
-            ret = np.array([1,0,0]) # left
-        case 5:
-            ret = np.array([0,0,-1]) # back
-    return ret*0.555
+    return uvpx
+def body_cornerUvsNormalized(self: BodyPart) -> list[tuple[float,float]]:
+    return [(u/64, 1-v/64) for (u,v) in body_cornerUvs(self)]
 
 def mulin(arr, scales):
     return np.array([(row * scales) for row in arr])
-def debugged(s):
-    print(s)
-    return s
 
-def generateUV2FaceidCode(cubeAndOriginPairs):
-    dump = ""
-    for dirid in range(6):
-        dump += f"    case {dirid}:\n"
-        for cubeId, (partname, cube, origin) in enumerate(cubeAndOriginPairs):
-            uvids = counterClockwiseCornerUVIds(dirid, 0)
-            uvId2Coord = corverUvs(origin, partname)
-            for uvid in uvids:
-                uvCoord = uvId2Coord[uvid]
-                faceId = cubeId*6 + dirid
-                uvu = int(uvCoord[0]*64)
-                uvv = int((1-uvCoord[1])*64)
-                dump += f"        if ((uvu == {uvu}) && (uvv == {uvv})) return {faceId};\n"
-        dump += "        return -1;\n"
-    with open("shadercode.txt", "w+") as f:
-        f.write(dump)
+PIX1 = np.array([
+    [1,1,1],
+    [0,1,1],
+    [0,0,1],
+    [1,0,1],
+    [1,1,0],
+    [0,1,0],
+    [0,0,0],
+    [1,0,0],
+]) - np.array([0.5,0.5,0.5])
 
-def generateAreaToFaceCode(cubeAndOriginPairs):
-    pixelgrid = [[f".   " for _ in range(64)] for _ in range(64)]
-    for dirid in range(6):
-        for cubeId, (partname, cube, origin) in enumerate(cubeAndOriginPairs):
-            uvids = counterClockwiseCornerUVIds(dirid, 0)
-            uvId2Coord = corverUvs(origin, partname)
-            a = [uvId2Coord[uvid] for uvid in uvids]
-            us, vs = zip(*a)
-            us = [int(u*64) for u in us]
-            vs = [int((1-v)*64) for v in vs]
-
-            umin, vmin = (min(us), min(vs))
-            umax, vmax = (max(us), max(vs))
-
-            faceId = cubeId*6 + dirid
-
-            for u in range(umin, umax):
-                for v in range(vmin, vmax):
-                    pixelgrid[v][u] = f"fi{faceId:>02}"
-
-    with open("cssgridcode.txt", "w+") as f:
-        for line in pixelgrid:
-            f.write(f'\"{" ".join(line)}\"\n')
-
-# case 4
-#   if (uvx == {uv.x} && uvy == {uv.x}) return {faceid}
+def body_toCubeCoords(self: BodyPart):
+    """ returns a list of [X,Y,Z] in the order as the image above DIRECTION_2_VERTEXORDER """
+    return np.asarray(self.origin_3d_center) + mulin(PIX1, self.axis_sizes) * self.scale_3d
 
 run()
