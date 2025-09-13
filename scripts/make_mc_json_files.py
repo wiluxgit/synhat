4548847c287d3e4af0862872242d55be122c7506
Wiluxgit <wilux00@gmail.com>
2025-09-13 10:19:02 +0200

    1.21.6 support

--- scripts/make_mc_json_files.py
+++ scripts/make_mc_json_files.py
@@ -2,7 +2,7 @@ import json
 from sort_table import sortaReadableJson
 from datetime import datetime
 
-def make_mc_files():
+def make_mc_files_old():
     file = "__master_models_table.json"
 
     inFile = open(file)
@@ -51,9 +51,51 @@ def make_mc_files():
 
     print("done", datetime.now().strftime("%H:%M:%S"))
 
+def make_mc_files_21_4():
+    file = "__master_models_table.json"
+
+    fileData = json.load(open(file))
+    hatData = fileData["models"]
+    itemName2itemModel = {}
+    for declr in hatData:
+        nameSpacedItem = declr["item"]
+        itemName = nameSpacedItem.split(":")[-1]
+        data = declr["data"]
+        path = declr["model"]
+        if not itemName in itemName2itemModel:
+            vanillaPath = f"C:/Users/wilux/AppData/Roaming/ModrinthApp/profiles/synergy/resourcepacks/_1.21.8/items/{itemName}.json"
+            mc = json.load(open(vanillaPath))
+            itemName2itemModel[itemName] = {
+                "model": {
+                    "type": "range_dispatch",
+                    "property": "custom_model_data",
+                    "index": 0,
+                    "entries": [],
+                    "fallback": mc["model"],
+                }
+            }
+        itemName2itemModel[itemName]["model"]["entries"].append({
+            "threshold": data,
+            "model": {
+                "type": "model",
+                "model": path
+            }
+        })
+
+    for k,v in itemName2itemModel.items():
+        v["model"]["entries"].sort(
+            key=lambda x: x["threshold"]
+        )
+        jsonDump = sortaReadableJson(v)
+        with open(getMcItemModelPath(k),"w+") as f:
+            f.write(jsonDump)
+        print(k)
+
 
 def getMcModelPath(item):
     return f"../assets/minecraft/models/item/{item}.json"
+def getMcItemModelPath(item):
+    return f"../assets/minecraft/items/{item}.json"
 
 def get_cmdata(override):
     if "predicate" in override:
@@ -71,4 +113,7 @@ def cmdata_is_zero(override):
     return get_cmdata(override) == 0
 
 if __name__ == '__main__':
-    make_mc_files()
+    print("========================")
+    make_mc_files_old()
+    print("========================")
+    make_mc_files_21_4()
